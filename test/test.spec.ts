96fdfeaef104a0ed465307d41c248e0942fdf57e
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-03-19 12:40:01 +0530

    testing changes

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,47 +1,79 @@
-// NOTE: Queries are authomatically retried and don't fail (while calls do), so some query tests have been written as call tests.
-
-import { describe } from "mocha";
-import chai from "chai";
-const vite = require('@vite/vuilder');
-import chaiAsPromised from "chai-as-promised";
+// import { describe } from "mocha";
+import { expect } from "chai";
+const vuilder = require("@vite/vuilder");
 import config from "./vite.config.json";
-
-chai.use(chaiAsPromised);
-const expect = chai.expect;
 
 let provider: any;
 let deployer: any;
-let alice: any;
-let bob: any;
 let contract: any;
+let john: any;
+let jane: any;
 let mnemonicCounter = 1;
 
-describe('test SunkCost', function () {
-    beforeEach(async function () {
-        provider = vite.localProvider();
-        deployer = vite.newAccount(config.networks.local.mnemonic, 0);
-        // init users
-        alice = vite.newAccount(config.networks.local.mnemonic, mnemonicCounter++);
-        bob = vite.newAccount(config.networks.local.mnemonic, mnemonicCounter++);
-        await deployer.sendToken(alice.address, '0');
-        await alice.receiveAll();
-        await deployer.sendToken(bob.address, '0');
-        await bob.receiveAll();
+describe("test SunkCostGame", () => {
+  beforeEach(async function() {
+    provider = vuilder.newProvider(config.networks.local.http);
+    console.log(await provider.request("ledger_getSnapshotChainHeight"));
+    deployer = vuilder.newAccount(config.networks.local.mnemonic, 0, provider);
+    console.log('deployer', deployer.address);
+      
+    john = vuilder.newAccount(config.networks.local.mnemonic, mnemonicCounter++);
+    jane = vuilder.newAccount(config.networks.local.mnemonic, mnemonicCounter++);
+    await deployer.sendToken(john.address, '10');
+    await john.receiveAll();
+    await deployer.sendToken(jane.address, '10');
+    await jane.receiveAll();
+  });
+
+    it("test deploy game", async () => {
         // compile
-        const compiledContracts = await vite.compile('SunkCostGame.solpp',);
-        expect(compiledContracts).to.have.property('SunkCostGame');
-        contract = compiledContracts.SunkCost;
+        const compiledContracts = await vuilder.compile("SunkCostGame.solpp");
+        expect(compiledContracts).to.have.property("SunkCostGame");
+
         // deploy
+        contract = compiledContracts.SunkCostGame;
         contract.setDeployer(deployer).setProvider(provider);
-        await contract.deploy({params: [1000000000000000000], responseLatency: 1});
-        expect(contract.address).to.be.a('string');
+        await contract.deploy({params: [10], responseLatency: 1, amount: '1000'});
+        expect(contract.address).to.be.a("string");
+        console.log(contract.address);
     });
 
-    describe('Owner', function () {
-        it('Onwer Query Failed', async function () {
-            await expect(
-                contract.call('owner', [1], {caller: alice})
-            ).to.eventually.be.rejectedWith('revert');
-        });
+//     // check default balance
+//     expect(await cafe.balance()).to.be.equal('0');
+//     // check default value of data
+//     let result = await cafe.query("price", []);
+//     console.log("return", result);
+//     expect(result)
+//       .to.be.an("array")
+//       .with.lengthOf(1);
+//     expect(result![0]).to.be.equal("1000000000000000000");
+
+//     // call Cafe.buyCoffee(to, numOfCups);
+//     const block = await cafe.call(
+//       "buyCoffee",
+//       ["vite_3345524abf6bbe1809449224b5972c41790b6cf2e22fcb5caf", 2],
+//       { amount: "2000000000000000000" }
+//     );
+
+//     // console.log(block);
+//     const events = await cafe.getPastEvents('Buy', {fromHeight: block.height, toHeight: block.height});
+//     expect(events)
+//       .to.be.an("array")
+//       .with.lengthOf(1);
+//     expect(events[0]?.returnValues?.from).to.be.equal(deployer.address);
+//     expect(events[0]?.returnValues?.to).to.be.equal(
+//       "vite_3345524abf6bbe1809449224b5972c41790b6cf2e22fcb5caf"
+//     );
+//     expect(events[0]?.returnValues?.num).to.be.equal("2");
+
+//     expect(await cafe.balance()).to.be.equal('0');
+//   });
+});
+
+describe('Owner', function () {
+    it('Owner Query Failed', async function () {
+        await expect(
+            contract.call('owner', [1])
+        ).to.eventually.be.rejectedWith('revert');
     });
 });
